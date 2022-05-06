fed2ab0eba0b7379feb92b2431db852dc87576a5
elerdg <97630596+elerdg@users.noreply.github.com>
2022-05-06 09:33:55 +0200

    Update inference_ita.py

--- inference_ita.py
+++ inference_ita.py
@@ -20,7 +20,6 @@ import sys
 #"""import the model, processor, tokenizer"""
 
 print("loading saved model")
-#saved_model= torch.load("/data/disk1/data/erodegher/model-wav2vec2-ita")  #saved epoch 50
 saved_model = AutoModelForCTC.from_pretrained("/data/disk1/data/erodegher/wav2vec2-large-xls-r-300m-italian-colab/checkpoint-36000/", local_files_only = True)
 saved_model.to("cuda")
 
@@ -28,7 +27,6 @@ print("loading tokenizer")
 tokenizer =  Wav2Vec2CTCTokenizer.from_pretrained("/data/disk1/data/erodegher/wav2vec2-large-xls-r-300m-italian-colab/checkpoint-36000/", local_files_only = True)
 
 print("loading processor")
-#processor= torch.load("/data/disk1/data/erodegher/processor_wav2vec-it")
 processor = Wav2Vec2Processor.from_pretrained("/data/disk1/data/erodegher/wav2vec2-large-xls-r-300m-italian-colab/checkpoint-36000/", local_files_only = True)
 
 ## import test set 
@@ -51,22 +49,27 @@ def prepare_dataset(batch):
     return batch
 
 common_voice_test = common_voice_test.map(prepare_dataset, remove_columns=common_voice_test.column_names)
-
+common_voice_test= common_voice_test.filter(lambda x : x < 5.0*processor.feature_extractor.sampling_rate, input_columns=["input_length"])
 
 """# Evaluation"""
 print('evaluation')
-input_dict = processor(common_voice_test["input_values"], return_tensors="pt", padding=True)
-logits = saved_model(input_dict.input_values.cuda()).logits
-pred_ids = torch.argmax(logits, dim=-1)
-predicted_sentences = processor.decode(pred_ids)
+predictions = [ ]
+for el in common_voice_test["input_values"]:
+    input_dict = processor(el, return_tensors="pt", padding=True)
+    logits= saved_model(input_dict.input_values.cuda()).logits
+    #print(logits.shape)
+    pred_ids = torch.argmax(logits[0], dim=-1)
+    #print(pred_ids)
+    predicted_sentences = processor.decode(pred_ids)
+    predictions.append(predicted_sentences)
+    #print(predictions)
+    
+    for i, sentence_ in enumerate(predictions):
+        print("Prediction: ",  sentence_)
+        #print("Reference", common_voice_test[i]["sentence"])
 
-for i, predicted_sentence in enumerate(predicted_sentences):
-    print("-" * 100)
-    print("Reference:", common_voice_test[i]["sentence"])
-    print("Prediction:", predicted_sentence)
-
-print("WER: {:2f}".format(100 * wer.compute(predicted_sentence=result["pred_strings"], references=result["sentence"])))
-print("CER: {:2f}".format(100 * cer.compute(predicted_sentence=result["pred_strings"], references=result["sentence"])))
+#print("WER: {:2f}".format(100 * wer.compute(predicted_sentence=sentence_, references=common_voice_test[i]["sentence"])))
+#print("CER: {:2f}".format(100 * cer.compute(predicted_sentence=result["pred_strings"], references=result["sentence"])))
 
 
 
