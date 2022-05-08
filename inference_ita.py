af49a389ad5fe6d646d65edd5ac52d7c33737010
elerdg <97630596+elerdg@users.noreply.github.com>
2022-05-08 17:05:10 +0200

    Update inference_ita.py

--- inference_ita.py
+++ inference_ita.py
@@ -48,11 +48,18 @@ def prepare_dataset(batch):
         batch["labels"] = processor(batch["sentence"]).input_ids
     return batch
 
-common_voice_test = common_voice_test.map(prepare_dataset, remove_columns=common_voice_test.column_names)
-common_voice_test= common_voice_test.filter(lambda x : x < 5.0*processor.feature_extractor.sampling_rate, input_columns=["input_length"])
+common_voice_test = common_voice_test.map(prepare_dataset, remove_columns=common_voice_test.column_names )
+common_voice_test= common_voice_test.filter(lambda x : x < 5.0*16000, input_columns=["input_length"])
+
+"""#Loading original Transcriptions"""
+print("loading transcriptions")
+common_voice_transcription= load_dataset("common_voice", "it", split="test[:10%]")
+common_voice_transcription=common_voice_transcription.cast_column("audio", Audio(sampling_rate=16_000))
+transcription=[ el for el in common_voice_transcription if len(el["audio"]["array"]) < 5.0*16000]
 
 """# Evaluation"""
 print('evaluation')
+d_predictions={}
 predictions = [ ]
 for el in common_voice_test["input_values"]:
     input_dict = processor(el, return_tensors="pt", padding=True)
@@ -62,14 +69,19 @@ for el in common_voice_test["input_values"]:
     #print(pred_ids)
     predicted_sentences = processor.decode(pred_ids)
     predictions.append(predicted_sentences)
+
     #print(predictions)
-    
     for i, sentence_ in enumerate(predictions):
-        print("Prediction: ",  sentence_)
-        #print("Reference", common_voice_test[i]["sentence"])
+        #d_predictions[sentence_]= transcription[i]["sentence"]
+        print(i, "Sentence: ",  sentence_)
+        print(i, "Reference: ",  transcription[i]["sentence"])
 
-#print("WER: {:2f}".format(100 * wer.compute(predicted_sentence=sentence_, references=common_voice_test[i]["sentence"])))
-#print("CER: {:2f}".format(100 * cer.compute(predicted_sentence=result["pred_strings"], references=result["sentence"])))
+        result_cer= cer.compute(predictions=[sentence_], references=[transcription[i]["sentence"]])
+        result_wer= wer.compute(predictions=[sentence_], references=[transcription[i]["sentence"]])
 
-
-
+        d={ "predictions":[sentence_],
+            "reference":[transcription[i]["sentence"]],
+            "CER score":[result_cer],
+            "WER_score":[result_cer],
+           }
+        pd.DataFrame(data=d)
